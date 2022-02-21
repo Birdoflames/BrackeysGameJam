a875c48402ba5d0d7ae975277f55f71c0155dfe9
Birdoflames <drnmalka@gmail.com>
2022-02-21 20:53:00 +0200

    moving manualy

---
+++ Inventory.gd
@@ -0,0 +1,19 @@
+extends Control
+
+var invCoords = {Vector2(0, 0) : false, Vector2(15, 15) : false}
+var currentcoords = [Vector2(0, 0), Vector2(15, 15)]
+
+var items = ["ancient sword", "ancient bow", "Genie's lamp"]
+var recent_item = items[-1]
+
+
+
+
+func coords_gen():
+	var count = 0
+	
+	for i in invCoords:
+		if i == false:
+			recent_item.Vector2 = currentcoords[count]
+			print(recent_item.Vector2)
+		count += 1
