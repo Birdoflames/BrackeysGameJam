2b16a53401f00709773c41be53cd2521eee6a8ec
RonHabaron <61042506+barak112@users.noreply.github.com>
2022-02-22 20:59:41 +0200

    item system done

---
+++ Bow.gd
@@ -0,0 +1,7 @@
+extends Sprite
+
+func story():
+	var story = "the story"
+	return story
+
+
--- play.gd
+++ play.gd
@@ -1,10 +1,11 @@
 extends Control
 
-
-
+func _ready():
+	print(get_node("Label").get_text())
+	
 func _on_Play_pressed():
-	get_tree().change_scene("res://Game_Scene.tscn")
-	load("res://Game_Scene.gd").story_decider()
+	get_tree().change_scene_to(load("res://Game_Scene.tscn"))
+	
 	
 
 	
