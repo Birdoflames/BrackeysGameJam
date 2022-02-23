ab8ab933736103391ddb2f47b49f09f98a70f2dc
RonHabaron <61042506+barak112@users.noreply.github.com>
2022-02-23 17:06:37 +0200

    dictionary start

--- Bow.gd
+++ Bow.gd
@@ -1,7 +1,13 @@
 extends Sprite
 
 func story():
-	var story = "the story"
+	var years_ago = "15"
+	var story = "we died" + years_ago + "year ago"
+	
+	var dictionary_story = "they died " + years_ago + "years_ago"
+	
+	get_parent().get_node("GUI/Artifacts_book/TextureRect/ancient bow label").set_text(dictionary_story)
 	return story
 
-
+func _ready():
+	story()
