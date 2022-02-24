a0c4801db002e46e0b4f403bb52e6d34064d67c3
RonHabaron <61042506+barak112@users.noreply.github.com>
2022-02-24 15:24:26 +0200

    starting 2 of dictionary

--- Dictonary.gd
+++ Dictonary.gd
@@ -10,11 +10,9 @@ onready var timer = get_node("ancient bow Label/Timer")
 
 func _on_dictionary_button_pressed():
 	get_tree().change_scene_to(load("res://Dictonary.tscn"))
-	timer.set_wait_time(2)
-	timer.start()
-	print(timer.get_time_left())
+#	timer.set_wait_time(2)
+#	timer.start()
+#	print(timer.get_time_left())
 
 func _on_Timer_timeout():
-
-
 	get_node("ancient bow Label").show()
